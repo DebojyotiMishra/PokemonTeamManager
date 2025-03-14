f7136953b068a3f6da8e6c3558e2d8b964e985be
Debojyoti Mishra <105543436+DebojyotiMishra@users.noreply.github.com>
2025-03-14 17:23:03 +0100

    feat(tests): create setup.js for test environment

---
+++ src/tests/setup.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const { MongoMemoryServer } = require('mongodb-memory-server');
+
+let mongoServer;
+
+beforeAll(async () => {
+  mongoServer = await MongoMemoryServer.create();
+  const mongoUri = mongoServer.getUri();
+  
+  await mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+});
+
+beforeEach(async () => {
+  const collections = mongoose.connection.collections;
+  for (const key in collections) {
+    const collection = collections[key];
+    await collection.deleteMany();
+  }
+});
+
+afterAll(async () => {
+  await mongoose.connection.close();
+  await mongoServer.stop();
+});
+
+process.env.NODE_ENV = 'test';
